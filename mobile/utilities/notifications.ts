84fb1f5280606b3014e4cd4dededd2fb448bc659
MQInfer12 <maummq@gmail.com>
2023-09-22 15:02:38 -0400

    Aplicacion en producción con notificaciones funcionando

--- mobile/utilities/notifications.ts
+++ mobile/utilities/notifications.ts
@@ -34,6 +34,16 @@ export async function sendPushNotification(expoPushToken: string) {
 
 export async function registerForPushNotificationsAsync() {
   let token;
+
+  if (Platform.OS === 'android') {
+    await Notifications.setNotificationChannelAsync('default', {
+      name: 'default',
+      importance: Notifications.AndroidImportance.MAX,
+      vibrationPattern: [0, 250, 250, 250],
+      lightColor: '#FF231F7C',
+    });
+  }
+
   if (Device.isDevice) {
     const { status: existingStatus } = await Notifications.getPermissionsAsync();
     let finalStatus = existingStatus;
@@ -50,15 +60,6 @@ export async function registerForPushNotificationsAsync() {
     });
   } else {
     alert('Debes usar un dispositivo físico para recibir notificaciones');
-  }
-
-  if (Platform.OS === 'android') {
-    Notifications.setNotificationChannelAsync('default', {
-      name: 'default',
-      importance: Notifications.AndroidImportance.MAX,
-      vibrationPattern: [0, 250, 250, 250],
-      lightColor: '#FF231F7C',
-    });
   }
 
   return token;
